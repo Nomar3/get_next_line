dd34a2da104ba469af14352c83ab1b416d903276
Nomar3 <151463475+Nomar3@users.noreply.github.com>
2024-01-25 23:26:36 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rmarin-j <rmarin-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 20:38:29 by rmarin-j          #+#    #+#             */
-/*   Updated: 2024/01/22 20:41:18 by rmarin-j         ###   ########.fr       */
+/*   Updated: 2024/01/25 22:26:41 by rmarin-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	new_line(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
+	while (str[i] != '\0'){
+		if (str[i] == '\n')
+			return (i);
 		i++;
-	if (str[i] == '\n')
-		return (i);
+	}
 	return (-1);
 }
 
@@ -31,7 +32,10 @@ char	*line_cut(char *storage)
 	char	*pre_n;
 	
 	i = 0;
-	
+	while(storage[i] && storage[i]!='\n')
+		i++;
+	pre_n = malloc(i+1);
+	i = 0;
 	while(storage[i])
 	{
 		pre_n[i] = storage[i];
@@ -39,15 +43,21 @@ char	*line_cut(char *storage)
 			return (pre_n);
 		i++;
 	}
-	
+	return (storage);
 }
 
-char	*ft_read(char *storage, int fd)
+char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE + 1];
+	static char* storage;
+		char	buffer[BUFFER_SIZE + 1];
 	char	*temp;
 	int		n_byte;
 
+	if (!storage)
+	{
+		storage = malloc(1);
+		*storage = 0;
+	} 
 	n_byte = read(fd, buffer, BUFFER_SIZE);
 	buffer[n_byte] = 0;
 	//si falla read o (ha llegado al final y el storage esta vacio)
@@ -66,23 +76,22 @@ char	*ft_read(char *storage, int fd)
 		temp = ft_strjoin(storage, buffer);
 		free(storage);
 		storage = temp;
-		return (ft_read(storage, fd));
+		return (get_next_line(fd));
 	}
 	//si tiene salto de linea, 
 	else
-		return (storage);
-}
-
-
-char	*get_next_line(int fd)
-{
-	static char* storage;
-	int	n;
-
-	if (!storage)
 	{
-		storage = malloc(sizeof(char));
-		*storage = 0;
+		storage = line_cut(buffer);
+		return (storage);
 	}
-	storage = ft_read(storage, fd);
+}
+int main()
+{
+	int fd;
+	
+	fd = open("texto.txt", O_RDONLY);
+	printf("%s\n\n", get_next_line(fd));
+/* 	printf("%s\n\n", get_next_line(fd));
+	printf("%s\n\n", get_next_line(fd));
+	printf("%s\n\n", get_next_line(fd)); */
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,30 @@
 /*   By: rmarin-j <rmarin-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 20:41:02 by rmarin-j          #+#    #+#             */
-/*   Updated: 2024/01/21 21:08:49 by rmarin-j         ###   ########.fr       */
+/*   Updated: 2024/01/25 22:18:36 by rmarin-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 5
 # endif
 
 # include <unistd.h>
 # include <stddef.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
+
 
 char	*get_next_line(int fd);
+char	*ft_read(char *storage, int fd);
+char	*line_cut(char *storage);
+int		new_line(char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strncmp(const char *s1, const char *s2);
+size_t	ft_strlen(const char *str);
 
 #endif
